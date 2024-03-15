cd20c23b0167d225f5ba899f4c247a6501de35fe
bmonish <monishmonib@gmail.com>
2024-03-16 01:15:52 +0530

    feat: Added Option to Encode / Decode Base 64

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const client = new Client({
   intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
 
-client.on("ready", async (c) => {
+client.on(Events.ClientReady, async (c) => {
   console.log(`Logged in as ${c.user.tag}`);
 
   client.guilds.cache.forEach(async (guild) => {
@@ -15,11 +15,21 @@ client.on("ready", async (c) => {
     if (!base64Command) {
       await guild.commands.create({
         name: "base64",
-        description: "Encode text to base64",
+        description: "Encode / Decode text to base64",
         options: [
           {
+            name: "operation",
+            description: "Whether to encode / decode the text",
+            required: true,
+            type: 3,
+            choices: [
+              { name: "Encode", value: "encode" },
+              { name: "Decode", value: "decode" },
+            ],
+          },
+          {
             name: "text",
-            description: "The text to encode",
+            description: "The text to encode / decode",
             required: true,
             type: 3,
           },
@@ -29,30 +39,24 @@ client.on("ready", async (c) => {
   });
 });
 
-client.on("messageCreate", (message) => {
-  if (message.content === "!ping") {
-    message.reply("Pong!");
-  }
-
-  if (message.content.startsWith("!base64")) {
-    const text = message.content.split(" ").slice(1).join(" ");
-    const encoded = Buffer.from(text).toString("base64");
-    message.reply(encoded);
-  }
-});
-
-// I want to listen to the base64 command registered above and reply with the encoded text
-client.on("interactionCreate", async (interaction) => {
+client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const { commandName, options } = interaction;
 
   if (commandName === "base64") {
+    const operationOption = options.get("operation");
     const textOption = options.get("text");
-    if (textOption) {
+    if (operationOption && textOption) {
+      const operation = operationOption.value as string;
       const text = textOption.value as string;
-      const encoded = Buffer.from(text).toString("base64");
-      interaction.reply(encoded);
+      if (operation === "decode") {
+        const decoded = Buffer.from(text, "base64").toString("utf-8");
+        interaction.reply(decoded);
+      } else {
+        const encoded = Buffer.from(text).toString("base64");
+        interaction.reply(encoded);
+      }
     }
   }
 });
